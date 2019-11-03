109894afadfe282b16ebe68735c4efc89532a83c
Andrey Akimov <Akimovan001@gmail.com>
2019-11-03 14:29:08 +0400

    create project

---
+++ app/src/main/java/com/akimov/simbirsoft_intepship/MainActivity.java
@@ -0,0 +1,13 @@
+package com.akimov.simbirsoft_intepship;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
